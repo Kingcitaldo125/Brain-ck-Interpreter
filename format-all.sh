a638a4d8332bcd5a044390c762358688934eaf20
Paul Arelt <paularelt@gmail.com>
2025-04-07 18:31:49 -0400

    pare down format code

--- format-all.sh
+++ format-all.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
 
-# Path to clang-format (can also be just 'clang-format' if it's in PATH)
-CLANG_FORMAT=clang-format
-
 # Find and format all .cpp and .hpp files recursively
 find . -type f \( -name "*.cpp" -o -name "*.hpp" \) -print0 | while IFS= read -r -d '' file; do
-    "$CLANG_FORMAT" -i "$file"
+    clang-format -i "$file"
 done
