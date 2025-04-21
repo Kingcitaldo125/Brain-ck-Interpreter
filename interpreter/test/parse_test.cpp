c1c6df5d3ac4bf651bbf7756c3e4c63c3944ed1c
Paul Arelt <paularelt@gmail.com>
2025-04-21 15:08:09 -0400

    Add more tests

--- interpreter/test/parse_test.cpp
+++ interpreter/test/parse_test.cpp
@@ -1,8 +1,39 @@
 #include <catch2/catch_test_macros.hpp>
 #include "interpreter.hpp"
 
+const char* test1 = "This is a test Brainf*ck script written" \
+"for Coding Challenges!" \
+"++++++++++[>+>+++>+++++++>++++++++++<<<" \
+"<-]>>>++.>+.+++++++..+++.<<++++++++++++" \
+"++.------------.>-----.>.-----------.++" \
+"+++.+++++.-------.<<.>.>+.-------.+++++" \
+"++++++..-------.+++++++++.-------.--.++" \
+"++++++++++++. What does it do?";
+
+const char* test2 = "++++++++++[>+>+++>+++++++>++++++++++<<<<-]>>>++.>+.+++++++..+++.<<+++.";
+const char* test3 = "++++++++[>++++[>++>+++>+++>+<<<<-]>+>+>->>+[<]<-]" \
+">>.>---.+++++++..+++.>>.<-.<.+++.------.--------.>>+.>++.";
+
 TEST_CASE("Empty Parse", "[parse]")
 {
     BrainFck::Parser parser;
     REQUIRE(0 == parser.parse(""));
+}
+
+TEST_CASE("Hello Coding Challenges", "[parse]")
+{
+    BrainFck::Parser parser;
+    REQUIRE(0 == parser.parse(test1));
+}
+
+TEST_CASE("Hello!", "[parse]")
+{
+    BrainFck::Parser parser;
+    REQUIRE(0 == parser.parse(test2));
+}
+
+TEST_CASE("Hello World!", "[parse]")
+{
+    BrainFck::Parser parser;
+    REQUIRE(0 == parser.parse(test3));
 }
